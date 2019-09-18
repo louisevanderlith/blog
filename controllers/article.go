4615ce236aa722a25fb239f1de8ac953264924ce
Louise van der Lith <louisevanderlith@gmail.com>
2019-09-18 19:40:49 +0200

    updated modules

--- controllers/article.go
+++ controllers/article.go
@@ -73,8 +73,14 @@ func (req *Article) Create(ctx context.Requester) (int, interface{}) {
 // @Failure 403 body is empty
 // @router / [put]
 func (req *Article) Update(ctx context.Requester) (int, interface{}) {
+	key, err := husk.ParseKey(ctx.FindParam("key"))
+
+	if err != nil {
+		return http.StatusBadRequest, err
+	}
+
 	body := &core.Article{}
-	key, err := ctx.GetKeyedRequest(body)
+	err = ctx.Body(body)
 
 	if err != nil {
 		return http.StatusBadRequest, err
