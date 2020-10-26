5ee2e6a98b46b6a8247c5ea8959e1a024b4d18d7
louisevanderlith <louisevanderlith@gmail.com>
2020-10-26 13:00:22 +0200

    Added API, moved main to cmd

--- handles/handler.go
+++ handles/handler.go
@@ -1,40 +1,34 @@
 package handles
 
 import (
-	"github.com/louisevanderlith/kong/middle"
+	"github.com/louisevanderlith/droxolite/open"
 	"net/http"
 
 	"github.com/gorilla/mux"
 	"github.com/rs/cors"
 )
 
-func SetupRoutes(scrt, securityUrl, managerUrl string) http.Handler {
+func SetupRoutes(audience, issuer string) http.Handler {
 	r := mux.NewRouter()
-	ins := middle.NewResourceInspector(http.DefaultClient, securityUrl, managerUrl)
-	get := ins.Middleware("blog.articles.search", scrt, GetArticles)
-	r.HandleFunc("/articles", get).Methods(http.MethodGet)
+	mw := open.BearerMiddleware(audience, issuer)
+	r.Handle("/articles", mw.Handler(http.HandlerFunc(GetArticles))).Methods(http.MethodGet)
+	r.Handle("/articles/{key:[0-9]+\\x60[0-9]+}", mw.Handler(http.HandlerFunc(ViewArticle))).Methods(http.MethodGet)
 
-	view := ins.Middleware("blog.articles.view", scrt, ViewArticle)
-	r.HandleFunc("/articles/{key:[0-9]+\\x60[0-9]+}", view).Methods(http.MethodGet)
+	r.Handle("/articles/{pagesize:[A-Z][0-9]+}", mw.Handler(http.HandlerFunc(SearchArticles))).Methods(http.MethodGet)
+	r.Handle("/articles/{pagesize:[A-Z][0-9]+}/{hash:[a-zA-Z0-9]+={0,2}}", mw.Handler(http.HandlerFunc(SearchArticles))).Methods(http.MethodGet)
 
-	srch := ins.Middleware("blog.articles.search", scrt, SearchArticles)
-	r.HandleFunc("/articles/{pagesize:[A-Z][0-9]+}", srch).Methods(http.MethodGet)
-	r.HandleFunc("/articles/{pagesize:[A-Z][0-9]+}/{hash:[a-zA-Z0-9]+={0,2}}", srch).Methods(http.MethodGet)
+	r.Handle("/articles", mw.Handler(http.HandlerFunc(CreateArticle))).Methods(http.MethodPost)
 
-	create := ins.Middleware("blog.articles.create", scrt, CreateArticle)
-	r.HandleFunc("/articles", create).Methods(http.MethodPost)
+	r.Handle("/articles", mw.Handler(http.HandlerFunc(UpdateArticle))).Methods(http.MethodPut)
 
-	update := ins.Middleware("blog.articles.update", scrt, UpdateArticle)
-	r.HandleFunc("/articles", update).Methods(http.MethodPut)
+	//lst, err := middle.Whitelist(http.DefaultClient, securityUrl, "blog.articles.view", scrt)
 
-	lst, err := middle.Whitelist(http.DefaultClient, securityUrl, "blog.articles.view", scrt)
-
-	if err != nil {
-		panic(err)
-	}
+	//if err != nil {
+	//	panic(err)
+	//}
 
 	corsOpts := cors.New(cors.Options{
-		AllowedOrigins: lst,
+		AllowedOrigins: []string{"*"},
 		AllowedMethods: []string{
 			http.MethodGet,
 			http.MethodPost,
